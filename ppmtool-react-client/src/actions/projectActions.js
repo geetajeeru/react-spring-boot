451a3880292a05faea5741d18de322094378c3d6
geetajeeru <geethajeeru@gmail.com>
2020-07-05 17:51:29 +0530

    dashboard dynamic changes

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {GET_ERRORS} from "./tyes";
+import {GET_ERRORS, GET_PROJECTS} from "./tyes";
 
 export const createProject = (project, history) => async dispatch => {
     try{
@@ -11,4 +11,16 @@ export const createProject = (project, history) => async dispatch => {
             payload: error.response.data
         });
     }
-}
+};
+
+export const getAllProjects = () => async dispatch => {
+    try{
+        const response = await axios.get("http://localhost:8080/api/project/all");
+        dispatch({
+            type: GET_PROJECTS,
+            payload: response.data
+        });
+    }catch(error){
+
+    }
+};
