8d84c5fdf9da90b9fefe08cbc918e23d6b410382
geetajeeru <geethajeeru@gmail.com>
2020-07-05 15:24:09 +0530

    redux integration and add project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/layout/Header';
 import Dashboard from './components/Dashboard';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProject from './components/project/AddProject';
+import {Provider} from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Dashboard/>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/addProject" component={AddProject}/>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
