fd8eb6fc93b6f8ff5f94d7ccffc4c1739eec8b72
geetajeeru <geethajeeru@gmail.com>
2020-07-06 15:04:42 +0530

    update and delete operations

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-import {GET_ERRORS, GET_PROJECTS} from "./tyes";
+import {GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT} from "./tyes";
 
 export const createProject = (project, history) => async dispatch => {
     try{
-        const response = await axios.post("http://localhost:8080/api/project", project);
+        const response = await axios.post("/api/project", project);
         history.push("/dashboard");
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
     }catch(error){
         dispatch({
             type: GET_ERRORS,
@@ -15,7 +19,7 @@ export const createProject = (project, history) => async dispatch => {
 
 export const getAllProjects = () => async dispatch => {
     try{
-        const response = await axios.get("http://localhost:8080/api/project/all");
+        const response = await axios.get("/api/project/all");
         dispatch({
             type: GET_PROJECTS,
             payload: response.data
@@ -24,3 +28,34 @@ export const getAllProjects = () => async dispatch => {
 
     }
 };
+
+export const getProject = (id, history) => async dispatch => {
+    try{
+        const response = await axios.get(`/api/project/${id}`);
+        dispatch({
+            type: GET_PROJECT,
+            payload: response.data
+        });
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    }catch(error){
+        history.push("/dashboard");
+    }
+};
+
+export const deleteProject = id => async dispatch => {
+    try{
+        if(window.confirm("Are you sure? This will delete the project and all the data related to it")){
+            const response = await axios.delete(`/api/project/${id}`);
+            dispatch({
+                type: DELETE_PROJECT,
+                payload: id
+            });
+        }
+        
+    }catch(error) {
+
+    }
+}
